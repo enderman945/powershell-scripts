88c2b8f8ba0dd2e5e40b45f6684bb44b8dae28e7
enderman945 <enderman.le.vrai@gmail.com>
2021-04-28 15:46:29 +0200

    continuation de autobackup

--- Auto-Backup.ps1
+++ Auto-Backup.ps1
@@ -20,21 +20,47 @@ do {
         Write-Host "$verif n'est pas une variable valide" }
 } until ( ($verif -eq "") -or ($verif -contains "y" ) )
 
-# $savefrom = "/home/guillmkde/Documents/test/projets/"
-# $saveto = "/home/guillmkde/Documents/test/Sauvegardes/"
+if (($savefrom -eq "") -and ($saveto -eq ""))
+{
+    $savefrom = "/home/env:USERNAME/Documents/Projets/"
+    $saveto = "/home/env:USERNAME/Documents/Sauvegardes/"
+}
+else {
+    
+}
+
 Set-Location $saveto
 Clear-Host
 Write-Host -ForegroundColor Green "Démarrage de la copie : "
 $date = Get-Date -Format "dd.MM.yyyy - HH.mm"
 Copy-Item -Path "$savefrom" -Destination "$saveto$date" -Recurse 
+$cperr = $?
+$cperrd = Get-Error
 Compress-Archive -Path "./$date" -DestinationPath "$date.zip"
 Remove-Item "$date" -Recurse
+if ($cperr -eq "True") {
+    $cperr = "Sauvegardé avec succés"
+}
+elseif ($cperr -eq "False") {
+    $cperr = "Erreur lors de la copie"
+    Clear-Host
+    Write-Host "$cperr"
+}
+else {
+    Write-Host "Erreur : révision du code nécéssaire"
+    $cperr = "Erreur : révision du code nécéssaire"
+}
 $detectlogfile = Get-ChildItem -File -Name logs.txt
 if ( $detectlogfile -eq "logs.txt" ) 
 { 
-    Add-Content
+    Add-Content -Path "./logs.txt" "$date : $cperr"
 }
 else {
     New-Item -ItemType File -Path "./logs.txt"
-    Add-Content
+    Add-Content -Path "./logs.txt" "$date : $cperr"
+}
+if ($cperrd -ne "" ) {
+  New-Item -ItemType Directory -Path "$saveto/Errors/"
+  New-Item -ItemType File -Path "$saveto/Errors/$date.log"
+  Add-Content -Path "$saveto/Errors/$date.log" "$cperrd"
 }
